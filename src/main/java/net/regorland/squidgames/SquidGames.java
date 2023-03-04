fb11ed4a0bea1fdd4feeeea9a0c83a1081705683
Ailakks <hola@cortmnzz.com>
2023-03-04 02:00:41 +0100

    initial commit

---
+++ src/main/java/net/regorland/squidgames/SquidGames.java
@@ -0,0 +1,17 @@
+package net.regorland.squidgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SquidGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
