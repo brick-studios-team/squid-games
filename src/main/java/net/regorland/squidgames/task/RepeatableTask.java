67a40ad0f5536562b172f591e7051f8b8abde0a1
Ailakks <hola@cortmnzz.com>
2023-03-04 16:19:24 +0100

    added advanced repeatable task

--- src/main/java/net/regorland/squidgames/task/RepeatableTask.java
+++ src/main/java/net/regorland/squidgames/task/RepeatableTask.java
@@ -5,11 +5,26 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 import net.regorland.squidgames.SquidGames;
 
+import java.util.function.Consumer;
+
 @Accessors(chain = true)
 public class RepeatableTask {
-    @Getter @Setter private Runnable end;
+    @Getter private static int bukkitTaskId = 0;
 
-    public RepeatableTask(Runnable runnable, int start, int delay) {
-        SquidGames.getInstance().getServer().getScheduler().scheduleSyncRepeatingTask(SquidGames.getInstance(), runnable, start, delay);
+    @Getter @Setter private Runnable end;
+    @Getter @Setter private int repetitions;
+
+    public RepeatableTask(Consumer<Integer> consumer, int start, int delay, int repetitions) {
+        this.repetitions = repetitions;
+
+        bukkitTaskId = SquidGames.getInstance().getServer().getScheduler().scheduleSyncRepeatingTask(SquidGames.getInstance(), () -> {
+            consumer.accept(this.repetitions);
+            this.repetitions--;
+
+            if (repetitions <= 0) {
+                SquidGames.getInstance().getServer().getScheduler().cancelTask(bukkitTaskId);
+                this.end.run();
+            }
+        }, start, delay);
     }
 }
