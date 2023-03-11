5c013776381919d0363bf6c02051830231a92942
Ailakks <hola@cortmnzz.com>
2023-03-11 18:42:03 +0100

    added game command

--- src/main/java/net/regorland/squidgames/SquidGames.java
+++ src/main/java/net/regorland/squidgames/SquidGames.java
@@ -1,10 +1,11 @@
 package net.regorland.squidgames;
 
+import co.aikar.commands.PaperCommandManager;
 import lombok.Getter;
 import net.regorland.squidgames.arena.Arena;
+import net.regorland.squidgames.commands.GameCommand;
 import net.regorland.squidgames.configuration.Configuration;
 import net.regorland.squidgames.configuration.ConfigurationManager;
-import net.regorland.squidgames.game.GameType;
 import net.regorland.squidgames.player.manager.GamePlayerManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,6 +15,7 @@ public final class SquidGames extends JavaPlugin {
 
     @Getter private Arena arena;
 
+    @Getter private PaperCommandManager paperCommandManager;
     @Getter private ConfigurationManager configurationManager;
     @Getter private GamePlayerManager gamePlayerManager;
 
@@ -25,8 +27,11 @@ public final class SquidGames extends JavaPlugin {
 
         this.arena = new Arena();
 
+        this.paperCommandManager = new PaperCommandManager(this);
         this.configurationManager = new ConfigurationManager();
         this.gamePlayerManager = new GamePlayerManager();
+
+        this.paperCommandManager.registerCommand(new GameCommand());
 
         this.locationsConfiguration = this.configurationManager.getConfig("locations.yml");
     }
---
+++ src/main/java/net/regorland/squidgames/commands/GameCommand.java
@@ -0,0 +1,31 @@
+/**
+ * This code has been programmed by Ailakks.
+ * Please, leave this note and give me credits
+ * in any project in which it is used. Have a nice day!
+ *
+ * @author : Ailakks
+ * @mailto : hola@ailakks.com
+ * @created : 11/03/2023
+ */
+
+package net.regorland.squidgames.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Subcommand;
+import lombok.RequiredArgsConstructor;
+import net.regorland.squidgames.SquidGames;
+import net.regorland.squidgames.game.GameType;
+import org.bukkit.entity.Player;
+
+@CommandAlias("game")
+@CommandPermission("squid-games.command.admin")
+@RequiredArgsConstructor
+public class GameCommand extends BaseCommand {
+
+    @Subcommand("phase start")
+    public void startPhase(Player player, GameType gameType) {
+        SquidGames.getInstance().getArena().setGameType(gameType).start();
+    }
+}
